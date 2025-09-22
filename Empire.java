a2c852d26fe51a50ee88f95bdb14c5099970774b
alaanvv <valealan84@gmail.com>
2025-09-22 11:33:27 -0300

    feat: building methods

--- Empire.java
+++ Empire.java
@@ -8,6 +8,29 @@ public class Empire {
   private int population = 3;
 
   private LumberCamp lumber_camp = new LumberCamp();
-  private ArrayList<Mine> mines  = new ArrayList<>();
   private ArrayList<Farm> farms  = new ArrayList<>();
+  private ArrayList<Mine> mines  = new ArrayList<>();
+
+  private int farm_count = 0;
+  private int mine_count = 0;
+  
+  // ---
+
+  public int build_farm() {
+    if (wood < 5 || gold < 2) return 0;
+    wood -= 5;
+    gold -= 2;
+
+    farms.add(new Farm(farm_count++));
+    return 1;
+  }
+  
+  public int build_mine() {
+    if (wood < 15 || gold < 5) return 0;
+    wood -= 15;
+    gold -= 5;
+
+    mines.add(new Mine(mine_count++));
+    return 1;
+  }
 }
