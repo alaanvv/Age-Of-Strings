265c0b89bd0f4f126d28238160e522ffeb3b0376
Cristopher Res. <cristophermoto27@gmail.com>
2025-11-23 13:25:07 -0300

    run turn button

--- src/Programa.java
+++ src/Programa.java
@@ -1,11 +1,9 @@
-import Interface.Interface;
 import persistencia.BancoDeDados;
 import visao.Terminal;
 
 public class Programa {
   public static void main(String[] args) {
     BancoDeDados db = new BancoDeDados();
-    //Terminal.mainMenu(db);
-    Interface game = new Interface(db);
+    Terminal.mainMenu(db);
   }
 }
